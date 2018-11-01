a49a16e56fd674dc210b8e009ef4f69fc439e763
Elena Polyakova <enterlina@gmail.com>
2018-11-01 16:32:17 +0300

    Update Nussin

--- HW3_Nussin/Alg_Nussin.py
+++ HW3_Nussin/Alg_Nussin.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-# Genome_1 = "AAACAUGAGGAUUACCCAUGU"
-# Genome_2 = "AAACAUGAGGAUUACCCAUGU"
+Genome_1 = "AAACAUGAGGAUUACCCAUGU"
+Genome_2 = "AAACAUGAGGAUUACCCAUGU"
 
 # Genome_1 = "GCACGACG"
 # Genome_2 = "GCACGACG"
 
-Genome_1 = "GGACC"
-Genome_2 = "GGACC"
+# Genome_1 = "GGACC"
+# Genome_2 = "GGACC"
 
 
 complement = {'A': 'U' , 'U': 'A' , 'C': 'G' , 'G': 'C'}
@@ -34,30 +34,25 @@ def max_row_column(index1 , index2 , M):
     return max_local
 
 
-for i in range ( 0 , max_length + 1 ):
-    M[ i ][ i ] = 0
+loop = 3
+for i in range ( max_length - loop - 2 , -1 , - 1 ):
+    for j in range ( i + loop + 1 , max_length ):
+        diag = 0
+        if complementary ( Genome_1[ i ] , Genome_2[ j ] ) == 'true':
+            diag = M[ i + 1 ][ j - 1 ] + 1
+        max_k = max_row_column ( i , j , M )
+        top = M[ i + 1 ][ j ]
+        left = M[ i ][ j - 1 ]
 
-for k in range ( 1 , len ( M ) +1 , 1 ):
-    for i in range ( 0 , len ( M )+1 , 1 ):
-        if (i + k) < len ( M ):
-            j = i + k
-            diag=0
-            if complementary ( Genome_1[ i ] , Genome_2[ j -1 ] ) == 'true':
-                diag = M[ i + 1 ][ j - 1 ] + 1
-            max_k = max_row_column ( i , j , M )
+        M[ i ][ j ] = max ( top , left , diag , max_k )
 
-            top = M[ i + 1 ][ j ]
-
-            left = M[ i ][ j - 1 ]
-
-            M[ i ][ j ] = max ( top , left , diag , max_k )
-
-            # print ( "ij" , i , j , "   Mij" , M[ i ][ j ] , "   top" , top ,
-            #         "   left" ,left , "   diag" , diag , "   Mi + 1/ j - 1  " , M[ i + 1 ][ j - 1 ] , "   Gen_1" ,
-            #         Genome_1[ i - 1 ] , "i-1" , i - 1 , "   Gen_2" , Genome_2[ j - 1 ] , "j-1" , j - 1 , "    max_pok" ,
-            #         max_row_column ( i , j , M ) )
+        # print ( "ij" , i , j , "   Mij" , M[ i ][ j ] , "   top" , top ,
+        #         "   left" ,left , "   diag" , diag , "   Mi + 1/ j - 1  " , M[ i + 1 ][ j - 1 ] , "   Gen_1" ,
+        #         Genome_1[ i - 1 ] , "i-1" , i - 1 , "   Gen_2" , Genome_2[ j - 1 ] , "j-1" , j - 1 , "    max_pok" ,
+        #         max_row_column ( i , j , M ) )
 
 print ( '\nMatrix M \n' , np.matrix ( M ) )
+
 
 def traceback(M , i , j , pairs):
     if j <= i:
@@ -67,7 +62,8 @@ def traceback(M , i , j , pairs):
     elif M[ i ][ j ] == M[ i ][ j - 1 ]:
         traceback ( M , i , j - 1 , pairs )
     elif complementary ( Genome_1[ i ] , Genome_2[ j ] ) and (M[ i ][ j ] == M[ i + 1 ][ j - 1 ] + 1):
-        pairs.append ( (i-1 , j) )
+        pairs.append ( (i , j) )
+        print ( Genome_1[ i ] , Genome_2[ j ] )
         traceback ( M , i + 1 , j - 1 , pairs )
     else:
         for k in range ( i + 1 , j ):
