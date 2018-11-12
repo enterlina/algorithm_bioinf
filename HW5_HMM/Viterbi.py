1acc496dafffae9a75fe35145d18f6cf233d7654
Elena Polyakova <enterlina@gmail.com>
2018-11-12 14:42:01 +0300

    Update algorithm

--- HW5_HMM/Viterbi.py
+++ HW5_HMM/Viterbi.py
@@ -8,9 +8,10 @@ sequence = 'ОРОРОРООРРРРРРРРРРОООООООО'
 sequence_back = [ [ 0 for col in range ( len ( sequence ) ) ] for row in range ( len ( sequence ) ) ]
 delta = [ [ 0 for col in range ( 0 , 2 ) ] for row in range ( len ( sequence ) ) ]
 d = [ [ 0 for col in range ( 0 , 4 ) ] for row in range ( len ( sequence ) ) ]
-
+f = [ [ 0 for col in range ( 0 , 4 ) ] for row in range ( len ( sequence ) ) ]
+g = [ [ 0 for col in range ( 0 , 4 ) ] for row in range ( len ( sequence ) ) ]
 alpha = [ [ 0 for col in range ( 0 , 2 ) ] for row in range ( len ( sequence ) ) ]
-beta = [ [ 0 for col in range ( 0 , 4 ) ] for row in range ( len ( sequence ) ) ]
+beta = [ [ 0 for col in range ( 0 , 2 ) ] for row in range ( len ( sequence ) ) ]
 
 pi = [ 0.5 , 0.5 ]
 # a = np.array ( [ [ 0.8 , 0.2 ] ,
@@ -81,56 +82,76 @@ def traceback(delta , a , b , d):
 
     return (result_str)
 
+
 print ( '\nResult\n' , " ".join ( traceback ( delta , a , b , d ) ) )
 
-def calculate_matrix_fb(delta , a , b , d):
+
+def calculate_matrix_fb(alpha , a , b , g):
     if sequence[ 0 ] == 'О':
-        delta[ 0 ][ 0 ] = pi[ 0 ] * b[ 0 ][ 0 ]
-        delta[ 0 ][ 1 ] = pi[ 1 ] * b[ 1 ][ 0 ]
+        alpha[ 0 ][ 0 ] = pi[ 0 ] * b[ 0 ][ 0 ]
+        alpha[ 0 ][ 1 ] = pi[ 1 ] * b[ 1 ][ 0 ]
         # print ('\nO', delta, 'b00',b[ 0 ][ 0 ],'b10',b[ 1 ][ 0 ])
     else:
-        delta[ 0 ][ 0 ] = pi[ 0 ] * b[ 0 ][ 1 ]
-        delta[ 0 ][ 1 ] = pi[ 1 ] * b[ 1 ][ 1 ]
+        alpha[ 0 ][ 0 ] = pi[ 0 ] * b[ 0 ][ 1 ]
+        alpha[ 0 ][ 1 ] = pi[ 1 ] * b[ 1 ][ 1 ]
     for i in range ( 1 , len ( sequence ) ):
         if sequence[ i ] == 'О':
-            d[ i ][ 0 ] = delta[ i - 1 ][ 0 ] * a[ 0 ][ 0 ] * b[ 0 ][ 0 ]
-            d[ i ][ 1 ] = delta[ i - 1 ][ 1 ] * a[ 0 ][ 1 ] * b[ 0 ][ 0 ]
-            d[ i ][ 2 ] = delta[ i - 1 ][ 0 ] * a[ 1 ][ 0 ] * b[ 1 ][ 0 ]
-            d[ i ][ 3 ] = delta[ i - 1 ][ 1 ] * a[ 1 ][ 1 ] * b[ 1 ][ 0 ]
+            g[ i ][ 0 ] = alpha[ i - 1 ][ 0 ] * a[ 0 ][ 0 ] * b[ 0 ][ 0 ]
+            g[ i ][ 1 ] = alpha[ i - 1 ][ 1 ] * a[ 0 ][ 1 ] * b[ 0 ][ 0 ]
+            g[ i ][ 2 ] = alpha[ i - 1 ][ 0 ] * a[ 1 ][ 0 ] * b[ 1 ][ 0 ]
+            g[ i ][ 3 ] = alpha[ i - 1 ][ 1 ] * a[ 1 ][ 1 ] * b[ 1 ][ 0 ]
 
         else:
-            d[ i ][ 0 ] = delta[ i - 1 ][ 0 ] * a[ 0 ][ 0 ] * b[ 0 ][ 1 ]
-            d[ i ][ 1 ] = delta[ i - 1 ][ 1 ] * a[ 0 ][ 1 ] * b[ 0 ][ 1 ]
-            d[ i ][ 2 ] = delta[ i - 1 ][ 0 ] * a[ 1 ][ 0 ] * b[ 1 ][ 1 ]
-            d[ i ][ 3 ] = delta[ i - 1 ][ 1 ] * a[ 1 ][ 1 ] * b[ 1 ][ 1 ]
+            g[ i ][ 0 ] = alpha[ i - 1 ][ 0 ] * a[ 0 ][ 0 ] * b[ 0 ][ 1 ]
+            g[ i ][ 1 ] = alpha[ i - 1 ][ 1 ] * a[ 0 ][ 1 ] * b[ 0 ][ 1 ]
+            g[ i ][ 2 ] = alpha[ i - 1 ][ 0 ] * a[ 1 ][ 0 ] * b[ 1 ][ 1 ]
+            g[ i ][ 3 ] = alpha[ i - 1 ][ 1 ] * a[ 1 ][ 1 ] * b[ 1 ][ 1 ]
 
-        delta[ i ][ 0 ] = d[ i ][ 0 ] + d[ i ][ 1 ]
-        delta[ i ][ 1 ] = d[ i ][ 2 ] + d[ i ][ 3 ]
+        alpha[ i ][ 0 ] = g[ i ][ 0 ] + g[ i ][ 1 ]
+        alpha[ i ][ 1 ] = g[ i ][ 2 ] + g[ i ][ 3 ]
 
-    # print ( 'd\n' , np.matrix ( d ))
-    # print ('delta\n',np.matrix(alpha))
-    return (delta)
+    return (alpha)
 
 
-print ( '\n',np.matrix(calculate_matrix_fb ( delta , a , b , d )) )
+calculate_matrix_fb ( alpha , a , b , g )
 
 
-def forwardback(delta , a , b , d):
+def forwardback(alpha , a , b , f):
     result_str = " "
-    k = 2
-    for i in range ( len ( sequence ) - 1 , 0 , -1 ):
-        if max ( d[ i ][ k ] , d[ i ][ k + 1 ] ) == d[ i ][ k ]:
-            result_str = 'T' + result_str
-            # print ( ' T result_str' , result_str , 'max' , max ( d[ i ][ k ] , d[ i ][ k ] ) , 'di0' , d[ i ][ k ] ,
-            #         'i' , i , 'k' , k )
-            k = 0
+
+    n = len ( sequence )
+    for i in range ( 0 , 2 ):
+        beta[ n - 1 ][ i ] = 1
+
+
+    for i in range ( 2 ):
+        beta[ n - 1 ][ i ] = 1
+
+    for i in range ( 1 , n ):
+
+        if sequence[ n - i ] == 'О':
+            f[ n - 1 - i ][ 0 ] = a[ 0 ][ 0 ] * beta[ n - i ][ 0 ] * b[ 0 ][ 0 ]
+            f[ n - 1 - i ][ 1 ] = a[ 1 ][ 0 ] * beta[ n - i ][ 1 ] * b[ 1 ][ 0 ]
+            f[ n - 1 - i ][ 2 ] = a[ 0 ][ 1 ] * beta[ n - i ][ 0 ] * b[ 0 ][ 0 ]
+            f[ n - 1 - i ][ 3 ] = a[ 1 ][ 1 ] * beta[ n - i ][ 1 ] * b[ 1 ][ 0 ]
+            beta[ n - 1 - i ][ 0 ] = f[ n - 1 - i ][ 0 ] + f[ n - 1 - i ][ 1 ]
+            beta[ n - 1 - i ][ 1 ] = f[ n - 1 - i ][ 2 ] + f[ n - 1 - i ][ 3 ]
         else:
-            result_str = 'F' + result_str
-            k = 2
-            # print ( ' F result_str' , result_str , 'max' , max ( d[ i ][ k ] , d[ i ][ k ] ) , 'di0' , d[ i ][ k ] ,
-            #         'i' , i , 'k' , k )
+            f[ n - 1 - i ][ 0 ] = a[ 0 ][ 0 ] * beta[ n - i ][ 0 ] * b[ 0 ][ 1 ]
+            f[ n - 1 - i ][ 1 ] = a[ 1 ][ 0 ] * beta[ n - i ][ 1 ] * b[ 1 ][ 1 ]
+            f[ n - 1 - i ][ 2 ] = a[ 0 ][ 1 ] * beta[ n - i ][ 0 ] * b[ 0 ][ 1 ]
+            f[ n - 1 - i ][ 3 ] = a[ 1 ][ 1 ] * beta[ n - i ][ 1 ] * b[ 1 ][ 1 ]
+            beta[ n - 1 - i ][ 0 ] = f[ n - 1 - i ][ 0 ] + f[ n - 1 - i ][ 1 ]
+            beta[ n - 1 - i ][ 1 ] = f[ n - 1 - i ][ 2 ] + f[ n - 1 - i ][ 3 ]
 
-    return (result_str)
+    result_T = [ 0 for i in range ( 0 , len ( sequence ) ) ]
+    result_F = [ 0 for i in range ( 0 , len ( sequence ) ) ]
+
+    print ( 'True' , '                False' )
+    for i in range ( 0 , len ( sequence ) ):
+        result_T[ i ] = (alpha[ i ][ 0 ] * beta[ i ][ 0 ]) / (alpha[ n - 1 ][ 0 ] + alpha[ n - 1 ][ 1 ])
+        result_F[ i ] = (alpha[ i ][ 1 ] * beta[ i ][ 1 ]) / (alpha[ n - 1 ][ 0 ] + alpha[ n - 1 ][ 1 ])
+        print ( result_T[ i ] , result_F[ i ] )
 
 
-# traceback ( delta , a , b , d )
+print ( forwardback ( alpha , a , b , f ) )
